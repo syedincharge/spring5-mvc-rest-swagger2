2de6f3316411cf614d1523f036fb2f3a30d16247
Lior Lavon <>
2020-05-22 12:20:53 +0200

    adding CircleCI

--- src/main/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplication.java
+++ src/main/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplication.java
@@ -1,4 +1,4 @@
-package springframework.guru.spring5mvcrest;
+package guru.springframework.spring5mvcrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
