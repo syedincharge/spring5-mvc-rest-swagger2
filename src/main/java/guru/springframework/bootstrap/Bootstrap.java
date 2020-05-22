c878eb8809c971e8f9ce93383080bcc8d2025267
Lior Lavon <>
2020-05-22 13:12:54 +0200

    remove CodeCov

--- src/main/java/guru/springframework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/bootstrap/Bootstrap.java
@@ -1,4 +1,45 @@
 package guru.springframework.bootstrap;
 
-public class Bootstrap {
+import guru.springframework.domain.Category;
+import guru.springframework.repositories.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category fruit = new Category();
+        fruit.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRepository.save(fruit);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        log.info("Data Loader finished : " + categoryRepository.count());
+    }
 }
--- src/main/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplication.java
+++ src/main/java/guru/springframework/spring5mvcrest/Spring5MvcRestApplication.java
@@ -2,8 +2,14 @@ package guru.springframework.spring5mvcrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"guru.*"})
+@EntityScan(basePackages = {"guru.*"})
+@EnableJpaRepositories(basePackages = {"guru.*"})
 public class Spring5MvcRestApplication {
 
 	public static void main(String[] args) {
