3a0737ae8005d31fb968e355ca129b6cc8526b3d
Lior Lavon <>
2020-05-22 17:46:18 +0200

    add MapStruct example

--- src/main/java/guru/springframework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/guru/springframework/api/v1/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package guru.springframework.api.v1.mapper;
 import guru.springframework.api.v1.model.CategoryDTO;
 import guru.springframework.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 
@@ -11,5 +12,6 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
